6f74fbe0adcea8a554ceb51e5a980b349e2c6b9b
Ahmed Ifham <66723461+ifham21@users.noreply.github.com>
2025-01-27 06:34:11 +0530

    updates

--- mymove/src/App.jsx
+++ mymove/src/App.jsx
@@ -1,35 +1,9 @@
-import { useState } from "react"
-
-const Card = ({title}) => {
-
-  const [hasLinked, setHasLiked] = useState(false);
-
-  return(
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLinked)}>
-        {hasLinked ? "💝" : '🤍'}
-      </button>
-    </div>
-  )
-}
-
+import React from 'react'
 
 const App = () => {
-
-  // anything start with 'use' in react is a hook
-
-  return(
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
+  return (
+    <div>App</div>
   )
-
 }
-
-
 
 export default App
