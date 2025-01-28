d8a66e599fc037e1f9ddd0a2ef5c12ee1b1f5de7
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-28 06:20:51 +0530

    Installed tailwindcss, added icons and images, added search component, all in all completed the header section

--- mymove/src/App.jsx
+++ mymove/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero banner" />
+          <h1>Find <span className="text-gradient" >Movies</span> You'll Enjoy without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
